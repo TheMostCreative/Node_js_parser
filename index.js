0342cdfe8317335b0435d0f301f410d0f8e1bc1d
Dmitriy Kolesnichenko <dimika2010@gmail.com>
2019-07-19 20:55:21 +0300

    Added regExp

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const fs = require('fs');
 var osmosis = require('osmosis');
 var savedData = [];
+
 osmosis
     .get('https://www.apple.com')
     .find('@href')
     .set('link')
     .log(console.log)
     .data(function (data) {
+        if (/^#/.test(data.link)) {
+            data.link = " https://www.apple.com/" + data.link; // etc.
+        }
+        else if (/^\//.test(data.link)){
+            data.link = " https://www.apple.com" + data.link; // etc.;
+        }
         console.log(data);
         savedData.push(data);
+
     })
     .done(function () {
-        fs.writeFile('data.json', JSON.stringify(savedData, null, 4) , function (err) {
+        fs.writeFile('data.json', JSON.stringify(savedData, null, 4), function (err) {
             if (err) console.error(err)
             else console.log('Data Saved to data.json file');
         })
