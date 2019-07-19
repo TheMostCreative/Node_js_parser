26ed78452dcaa57ac76236ad9a990420f4a7879e
Dmitriy Kolesnichenko <dimika2010@gmail.com>
2019-07-19 21:51:44 +0300

    added comments

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const fs = require('fs');
-var osmosis = require('osmosis');
+var osmosis = require('osmosis'); //пак для парсинга
 var savedData = [];
 
-osmosis
-    .get('https://www.apple.com')
-    .find('@href')
-    .set('link')
-    .log(console.log)
-    .data(function (data) {
-        if (/^#/.test(data.link)) {
+osmosis  
+    .get('https://www.apple.com') //берем ссылку
+    .find('@href') //выборка по элементам
+    .set('link') // пишем тип
+    .log(console.log) //вывод в консоль (особенности osmosis)
+    .data(function (data) {  //записываем данные которые взяли, регулярками делаем из относительного адреса абсолютные
+        if (/^#/.test(data.link)) {  
             data.link = " https://www.apple.com/" + data.link; // etc.
         }
         else if (/^\//.test(data.link)){
             data.link = " https://www.apple.com" + data.link; // etc.;
         }
         console.log(data);
-        savedData.push(data);
+        savedData.push(data); //добавляем в массив наши данные
 
     })
-    .done(function () {
+    .done(function () { //записываем в data.json наш массив savedData преобразовывая его в JSON
         fs.writeFile('data.json', JSON.stringify(savedData, null, 4), function (err) {
             if (err) console.error(err)
             else console.log('Data Saved to data.json file');
